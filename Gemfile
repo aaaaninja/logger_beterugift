055db4581c7aa7580d732192cc46c51b61bd3c73
k1nPr45b0 <keshigomu71505@gmail.com>
2019-02-10 07:16:39 +0000

    Add: 必要なファイル群をコミット
    動く環境さえ整っていればexec_browse.rbを実行するとselenium-chromiumがheadlessで動く
    なお、一度beterugift.jpのサイトに行って値段をsqlite3に記録するとすぐに終了する
    一回の実行につき一つのrecordとなる。

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+
+gem "sqlite3"
+gem "selenium-webdriver"
+
+gem "rspec"
+
+group :development do
+  gem "pry"
+  gem "pry-byebug"
+end
---
+++ exec_browse.rb
@@ -0,0 +1,39 @@
+Bundler.require(:default, :development)
+require "time"
+db = SQLite3::Database.new("Transition_beterugift.db")
+db.execute("CREATE TABLE IF NOT EXISTS gift_prices (id integer primary key, face_value integer, selling_price integer, discount_rate real, recording_date text)")
+stmt = db.prepare("INSERT INTO gift_prices(face_value, selling_price, discount_rate, recording_date) VALUES (?, ?, ?, ?)")
+
+
+Selenium::WebDriver::Chrome.driver_path = "/usr/lib/chromium-browser/chromedriver"
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1080, 1920")
+driver = Selenium::WebDriver.for :chrome, options: options
+
+
+driver.get("https://beterugift.jp/")
+sleep 5
+
+begin
+  cheapest_tr = driver.find_elements(xpath: '//*[@id="gift_table"]/tbody/tr')[0]
+                      .find_elements(css: "td")
+
+  face_value     = cheapest_tr[2].text
+                                 .delete("^0-9").to_f
+  selling_price  = cheapest_tr[3].text.split("\n")[0]
+                                 .delete("^0-9").to_f
+  discount_rate  = (selling_price / face_value).truncate(4)
+  recording_date = Time.now.getutc.iso8601
+  puts "#{face_value} #{selling_price} #{discount_rate} #{recording_date}"
+
+  stmt.execute(face_value, selling_price, discount_rate, recording_date)
+rescue => e
+  p e
+end
+
+
+stmt.close
+db.close
+driver.quit
